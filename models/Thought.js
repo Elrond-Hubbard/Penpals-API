48ab21a8a910f07a422f0433d6cd191210b0b535
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-11-01 11:47:38 -0400

    built template thought schema

---
+++ models/Thought.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+//TODO: add associated schemas here
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    min_length: 1,
+    max_length: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+});
+
+const Thought = model("Thought", thoughtSchema);
+
+module.exports = Thought;
