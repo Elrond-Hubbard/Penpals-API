17c04f58b5e7f4dec939da9bd7ba3b9580f72b8d
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-11-01 11:43:40 -0400

    moved user schema to correct file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const {connect, connection} = require('mongoose');
-const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/penpalsDB';
+const { connect, connection } = require("mongoose");
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/penpalsDB";
 
 connect(connectionString);
 
