62b19e1dd92470c5cc8798870313c0313fec6f43
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-11-02 18:26:11 -0400

    built CRUD routes for Thought model

--- models/User.js
+++ models/User.js
@@ -1,20 +1,32 @@
 const { Schema, model } = require("mongoose");
 // TODO: add associated schemas here
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    thoughts: [],
+    friends: [],
   },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  thoughts: [],
-  friends: [],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
 const User = model("User", userSchema);
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,3 +1,53 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const Thought = require("../../models/Thought");
+
+// Find all thoughts
+router.get("/", (req, res) => {
+  Thought.find({})
+    .then((data) => res.json(data))
+    .catch((err) => {
+      if (err) throw err;
+    });
+});
+
+// Find one thought by id
+router.get("/:id", (req, res) => {
+  Thought.find({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      if (err) throw err;
+    });
+});
+
+// Create one new thought and push to associated user
+router.post("/", (req, res) => {
+  Thought.create(req.body)
+    .then((data) => res.json(data))
+    .catch((err) => {
+      if (err) throw err;
+    });
+});
+
+// Update one thought by id
+router.put("/:id", (req, res) => {
+  Thought.findOneAndUpdate(
+    { _id: req.params.id },
+    { $set: req.body },
+    { new: true }
+  )
+    .then((data) => res.json(data))
+    .catch((err) => {
+      if (err) throw err;
+    });
+});
+
+// Delete one thought by id
+router.delete("/:id", (req, res) => {
+  Thought.deleteOne({ _id: req.params.id })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      if (err) throw err;
+    });
+});
 
 module.exports = router;
