20d8f2b454f85d1a803538e4a08eda304a69772e
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-11-02 18:27:32 -0400

    added virtuals to models

--- models/Thought.js
+++ models/Thought.js
@@ -1,22 +1,34 @@
 const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    min_length: 1,
-    max_length: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 const Thought = model("Thought", thoughtSchema);
